0c771e8d82e972390ab4cd3f2b3cd85520470756
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-20 23:59:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
- let count = 0;
-function incrementBtn() {
-      alert(count); 
-      count++; 
-      document.getElementById("counter").innerHTML = count;
-}
+let counter = 0;
+
+document.addEventListener("DOMContentLoaded", function(){
+	const btn= document.getElementById("incrementBtn");
+	let countElem=document.getElementById("counter");
+
+	btn.addEventListener("click", function(){
+		alert(counter);
+		counter++;
+		countElem.innerText=counter;
+	})
+})
